2db803aa64b9aed436034950ec05fec786ea9986
André Menezes <1992menezes@gmail.com>
2025-10-17 14:23:53 -0300

    Adicionar props onClick e type no componente Button

--- src/app/components/base/Button/index.tsx
+++ src/app/components/base/Button/index.tsx
@@ -6,15 +6,23 @@ import {
 } from 'react';
 
 export default function Button({
+  onClick,
+  type = 'button',
   children,
   StartIcon,
 }: PropsWithChildren<{
+  onClick?: () => void;
+  type?: 'button' | 'submit' | 'reset';
   StartIcon?: ForwardRefExoticComponent<
     Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>
   >;
 }>) {
   return (
-    <button className="flex items-center gap-2 px-4 py-2 rounded-md cursor-pointer">
+    <button
+      className="flex items-center gap-2 px-4 py-2 rounded-md cursor-pointer"
+      type={type}
+      onClick={onClick}
+    >
       {StartIcon && <StartIcon />}
       {children}
     </button>
