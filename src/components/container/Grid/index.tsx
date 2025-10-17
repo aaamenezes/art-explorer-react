907783deada3b1ed81b7c72020935f6d6d0646eb
André Menezes <1992menezes@gmail.com>
2025-10-17 17:35:00 -0300

    Criar componente Grid

---
+++ src/components/container/Grid/index.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react';
+
+export default function Grid({ children }: PropsWithChildren) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      {children}
+    </div>
+  );
+}
