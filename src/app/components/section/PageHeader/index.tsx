f5d0d01db667b4dbf4417ffe21badede7bd3c585
André Menezes <1992menezes@gmail.com>
2025-10-17 13:27:07 -0300

    Adicionar link para a home no logo

--- src/app/components/section/PageHeader/index.tsx
+++ src/app/components/section/PageHeader/index.tsx
@@ -4,7 +4,9 @@ import { Star } from 'lucide-react';
 export default function PageHeader() {
   return (
     <header className="flex justify-between items-center container mx-auto p-4 bg-amber-600">
-      <Logo />
+      <Link href="/">
+        <Logo />
+      </Link>
       <span>busca</span>
       <button>
         <Star /> <span>favoritar</span>
