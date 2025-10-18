5ef513f30dbdd60b60e80a69ceaf1618d23b9f57
André Menezes <1992menezes@gmail.com>
2025-10-18 11:11:41 -0300

    Criar estrutura inicial da página de detalhes de uma obra

--- src/app/artwork/[id]/page.tsx
+++ src/app/artwork/[id]/page.tsx
@@ -1,7 +1,15 @@
+import Wrapper from '@/components/container/Wrapper';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Art X</h1>
-    </main>
+    <Wrapper as="main">
+      <h1 className="text-2xl font-bold my-8">Detalhes da obra de ID 123!</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </Wrapper>
   );
 }
