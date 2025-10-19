423739cf1f5b710423a5c7743574ce5632de2659
André Menezes <1992menezes@gmail.com>
2025-10-18 22:54:14 -0300

    Inserir dado de figcaption vindo da API

--- src/components/block/Figure/index.tsx
+++ src/components/block/Figure/index.tsx
@@ -1,11 +1,11 @@
 import { FigureProps } from './types';
 
-export default function Figure({ children }: FigureProps) {
+export default function Figure({ children, figcaption }: FigureProps) {
   return (
     <figure className="flex flex-col gap-1">
       {children}
       <figcaption className="px-4 py-2 group-hover:underline">
-        figcaption
+        {figcaption}
       </figcaption>
     </figure>
   );
