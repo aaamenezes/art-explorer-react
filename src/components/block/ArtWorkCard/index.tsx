166ad2f1b0a8c90c9b120acba77929abc70108dd
André Menezes <1992menezes@gmail.com>
2025-10-19 13:24:19 -0300

    Favoritar e desfavoritar obras

--- src/components/block/ArtWorkCard/index.tsx
+++ src/components/block/ArtWorkCard/index.tsx
@@ -1,17 +1,38 @@
+'use client';
+
 import { buildArtWorkAltText } from '@/app/lib/buildArtWorkAltText';
+import {
+  addArtWorkToFavorites,
+  isArtWorkAlreadyFavorited,
+  removeArtWorkFromFavorites,
+} from '@/app/lib/favorites';
 import Button from '@/components/base/Button';
 import Image from '@/components/base/Image';
 import Link from '@/components/base/Link';
 import Figure from '@/components/block/Figure';
 import { Star } from 'lucide-react';
+import { useCallback, useState } from 'react';
 import { ArtWorkCardProps } from './types';
 
 export default function ArtWorkCard({ artWorkData }: ArtWorkCardProps) {
-  const currentArtIsFavorited = false;
+  const [currentArtWorkIsFavorited, setCurrentArtWorkIsFavorited] = useState(
+    isArtWorkAlreadyFavorited(artWorkData.objectID)
+  );
+
+  const handleFavorite = useCallback(() => {
+    if (currentArtWorkIsFavorited) {
+      removeArtWorkFromFavorites(artWorkData.objectID);
+      setCurrentArtWorkIsFavorited(false);
+      return;
+    }
+
+    addArtWorkToFavorites(artWorkData);
+    setCurrentArtWorkIsFavorited(true);
+  }, [currentArtWorkIsFavorited, artWorkData]);
 
   return (
     <article className="relative rounded-md overflow-hidden bg-neutral-100 dark:bg-neutral-900">
-      <Link href={artWorkData.objectURL} className="group">
+      <Link href={`/artwork/${artWorkData.objectID}`} className="group">
         <Figure figcaption={artWorkData.title}>
           <div className="overflow-hidden">
             <Image
@@ -30,8 +51,11 @@ export default function ArtWorkCard({ artWorkData }: ArtWorkCardProps) {
         </p>
         <p>{artWorkData.objectDate}</p>
       </div>
-      <Button className="absolute top-1 right-1 bg-white/40 hover:bg-white dark:bg-black/40 dark:hover:bg-black transition">
-        <Star className={currentArtIsFavorited ? 'fill-current' : ''} />
+      <Button
+        onClick={handleFavorite}
+        className="absolute top-1 right-1 bg-white/40 hover:bg-white dark:bg-black/40 dark:hover:bg-black transition"
+      >
+        <Star className={currentArtWorkIsFavorited ? 'fill-current' : ''} />
       </Button>
     </article>
   );
