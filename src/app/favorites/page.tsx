8d84c067bcedf7fffc931b1146be0f04a8457c59
André Menezes <1992menezes@gmail.com>
2025-10-18 23:47:19 -0300

    Criar componente de grid dos favoritos para isolar comportamentos de client component

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,17 +1,11 @@
-import ArtWorkCard from '@/components/block/ArtWorkCard';
-import Grid from '@/components/container/Grid';
 import Wrapper from '@/components/container/Wrapper';
+import FavoritesGrid from '@/components/section/FavoritesGrid';
 
-export default function Home() {
+export default function Favorites() {
   return (
     <Wrapper as="main">
       <h1 className="text-2xl font-bold my-8">Veja suas obras favoritas!</h1>
-      <Grid>
-        <ArtWorkCard objectID={418425} />
-        <ArtWorkCard objectID={418425} />
-        <ArtWorkCard objectID={418425} />
-        <ArtWorkCard objectID={418425} />
-      </Grid>
+      <FavoritesGrid />
     </Wrapper>
   );
 }
---
+++ src/components/section/FavoritesGrid/index.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { getFavoritesArtWorks } from '@/app/lib/favorites';
+import ArtWorkCard from '@/components/block/ArtWorkCard';
+import Grid from '@/components/container/Grid';
+
+export default function FavoritesGrid() {
+  const favoritesArtworks = getFavoritesArtWorks();
+
+  return (
+    <Grid>
+      {favoritesArtworks.map(favoriteArtWork => {
+        if (!favoriteArtWork.data.primaryImageSmall) return null;
+        return (
+          <ArtWorkCard
+            key={favoriteArtWork.data.objectID}
+            artWorkData={favoriteArtWork.data}
+          />
+        );
+      })}
+    </Grid>
+  );
+}
