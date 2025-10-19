1cceca3ebb998e2574d449b4d5ea2a3c6223d30b
André Menezes <1992menezes@gmail.com>
2025-10-19 13:11:59 -0300

    Criar métodos para manipular os favoritos no localStorage

---
+++ src/app/lib/favorites.ts
@@ -0,0 +1,67 @@
+'use client';
+
+import { ArtWorkProps } from '@/types/artwork';
+import { FavoriteArtWorkProps } from '@/types/favorite';
+
+const LOCAL_STORAGE_FAVORITES_KEY = 'met_favorites_artworks';
+
+export function addArtWorkToFavorites(artWorkToSave: ArtWorkProps) {
+  if (isArtWorkAlreadyFavorited(artWorkToSave.objectID)) return;
+
+  const favorites = getFavoritesArtWorks();
+
+  favorites.push({
+    id: artWorkToSave.objectID,
+    data: {
+      objectID: artWorkToSave.objectID,
+      title: artWorkToSave.title,
+      primaryImage: artWorkToSave.primaryImage,
+      primaryImageSmall: artWorkToSave.primaryImageSmall,
+      artistDisplayName: artWorkToSave.artistDisplayName,
+      artistDisplayBio: artWorkToSave.artistDisplayBio,
+      culture: artWorkToSave.culture,
+      objectDate: artWorkToSave.objectDate,
+      medium: artWorkToSave.medium,
+      department: artWorkToSave.department,
+      objectURL: artWorkToSave.objectURL,
+    },
+    updatedAt: new Date(),
+  });
+
+  window.localStorage.setItem(
+    LOCAL_STORAGE_FAVORITES_KEY,
+    JSON.stringify(favorites)
+  );
+}
+
+export function getFavoritesArtWorks(): FavoriteArtWorkProps[] {
+  if (typeof window === 'undefined') return [];
+
+  const favoritesStringOrNull = window.localStorage.getItem(
+    LOCAL_STORAGE_FAVORITES_KEY
+  );
+  if (!favoritesStringOrNull) return [];
+
+  return JSON.parse(favoritesStringOrNull);
+}
+
+export function isArtWorkAlreadyFavorited(objectID: number) {
+  const favorites = getFavoritesArtWorks();
+
+  const isArtWorkExistsInFavorites = favorites.find(
+    artWork => artWork.id === objectID
+  );
+
+  return Boolean(isArtWorkExistsInFavorites);
+}
+
+export function removeArtWorkFromFavorites(objectID: number) {
+  const favorites = getFavoritesArtWorks();
+
+  const updatedFavorites = favorites.filter(artWork => artWork.id !== objectID);
+
+  window.localStorage.setItem(
+    LOCAL_STORAGE_FAVORITES_KEY,
+    JSON.stringify(updatedFavorites)
+  );
+}
