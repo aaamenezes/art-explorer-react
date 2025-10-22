d0ad8c6173bb08fbd58e20a8154c7993f2f778a5
André Menezes <1992menezes@gmail.com>
2025-10-21 22:40:48 -0300

    Criar componente que expande/recolhe para reunir o filtros

--- src/components/container/Departments/index.tsx
+++ src/components/container/Departments/index.tsx
@@ -2,11 +2,13 @@ import Button from '@/components/base/Button';
 import { useArtworkStore } from '@/store/artworks';
 import { useDepartamentsStore } from '@/store/departments';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function Departments() {
   const router = useRouter();
   const searchParams = useSearchParams();
+
+  const [open, setOpen] = useState(false);
 
   const { loading, allDepartaments, loadAllDepartamentsFromApi } =
     useDepartamentsStore();
@@ -49,19 +51,33 @@ export default function Departments() {
 
   return (
     <div className="my-8">
-      <h2 className="text-2xl font-bold mb-4">Principais departamentos</h2>
-      <ul className="flex flex-row gap-2 flex-wrap">
-        {allDepartaments.map(department => (
-          <li key={department.displayName}>
-            <Button
-              className="inline-block p-2 border border-neutral-300 dark:border-neutral-700 rounded-md text-sm hover:bg-neutral-200 dark:hover:bg-neutral-800 transition-colors"
-              onClick={() => handleFilterDepartment(department.departmentId)}
-            >
-              {department.displayName}
-            </Button>
-          </li>
-        ))}
-      </ul>
+      <div className="flex justify-end">
+        <Button variant="secondary" onClick={() => setOpen(!open)}>
+          Filtros
+        </Button>
+      </div>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${
+          open ? 'max-h-40 opacity-100 mt-2' : 'max-h-0 opacity-0'
+        } bg-gray-50 dark:bg-gray-900 rounded-md`}
+      >
+        <div className="p-4">
+          <ul className="flex flex-row gap-2 flex-wrap">
+            {allDepartaments.map(department => (
+              <li key={department.displayName}>
+                <Button
+                  variant="secondary"
+                  onClick={() =>
+                    handleFilterDepartment(department.departmentId)
+                  }
+                >
+                  {department.displayName}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
