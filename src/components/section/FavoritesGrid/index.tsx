25047fc768f416739498bb8f8f8704a1c8ed2526
André Menezes <1992menezes@gmail.com>
2025-10-25 12:35:04 -0300

    Resolver problema de hydration nos favorites

--- src/components/section/FavoritesGrid/index.tsx
+++ src/components/section/FavoritesGrid/index.tsx
@@ -3,14 +3,23 @@
 import ArtWorkCard from '@/components/block/ArtWorkCard';
 import Grid from '@/components/container/Grid';
 import { useFavorite } from '@/hooks/useFavorite';
+import { FavoriteArtWorkProps } from '@/types/favorite';
+import { useEffect, useState } from 'react';
 
 export default function FavoritesGrid() {
-  const favorites = useFavorite();
+  const [favorites, setFavorites] = useState<FavoriteArtWorkProps[]>([]);
+
+  const favoritesHook = useFavorite();
+
+  useEffect(() => {
+    setFavorites(favoritesHook.getAll());
+  }, []);
 
   return (
     <Grid>
-      {favorites.getAll().map(favoriteArtWork => {
+      {favorites.map(favoriteArtWork => {
         if (!favoriteArtWork.data.primaryImageSmall) return null;
+
         return (
           <ArtWorkCard
             key={favoriteArtWork.data.objectID}
