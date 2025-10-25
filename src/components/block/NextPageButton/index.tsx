17cef77589016bb3334e59d67dfb01b66fa51b8f
André Menezes <1992menezes@gmail.com>
2025-10-25 19:22:23 -0300

    Adicionar ícones no botão de carregar próxima página

--- src/components/block/NextPageButton/index.tsx
+++ src/components/block/NextPageButton/index.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/base/Button';
 import { useArtworkStore } from '@/store/artworks';
+import { ArrowBigDownDash, LoaderPinwheel } from 'lucide-react';
 import { useCallback } from 'react';
 
 export default function NextPageButton() {
@@ -12,7 +13,17 @@ export default function NextPageButton() {
   return (
     <div className="flex justify-center my-10">
       <Button variant="primary" onClick={handleNextPage}>
-        {loading ? 'Carregando...' : 'Carregar mais'}
+        {loading ? (
+          <>
+            <LoaderPinwheel className="w-5 h-5 animate-spin" />
+            Carregando...
+          </>
+        ) : (
+          <>
+            <ArrowBigDownDash className="w-5 h-5 animate-bounce" />
+            Carregar mais
+          </>
+        )}
       </Button>
     </div>
   );
